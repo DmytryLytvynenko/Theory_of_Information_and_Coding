f6d602bc7cbadfa6e256a4c8c048d86d3e39c7ae
DmytryLytvynenko <dimdimalit@gmail.com>
2022-06-07 14:43:40 +0300

    Simple coding(Baudot code and ASCII)
    My first work with coding masseges

---
+++ TheoryOfInfandCode/TheoryOfInfandCode/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace TheoryOfInfandCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Alphabet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new TheoryOfInfandCode.DataSet1();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(519, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run ASCII";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 105);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(328, 75);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 198);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(328, 67);
+            this.textBox2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Alphabet,
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(35, 281);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(342, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Alphabet
+            // 
+            this.Alphabet.HeaderText = "Alphabet";
+            this.Alphabet.Name = "Alphabet";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Frequency";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Сount of inf";
+            this.Column2.Name = "Column2";
+            // 
+            // dataTable1BindingSource
+            // 
+            this.dataTable1BindingSource.DataMember = "DataTable1";
+            this.dataTable1BindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(612, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Draw Graph";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(397, 445);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Энтропия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(584, 445);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(584, 474);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(397, 474);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Количество инф в сообщении";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(584, 511);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(397, 511);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Средняя длина слова";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(584, 540);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(397, 540);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(117, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Коэф эффективности";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(584, 570);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(397, 570);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Коэф сжатия";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(49, 13);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(328, 76);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.Text = "Щаслива людина занадто задоволена сьогоденням, щоб занадто багато думати про майб" +
+    "утнє.";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(427, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Run Baudot";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 622);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dataTable1BindingSource;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Alphabet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TheoryOfInfandCode/TheoryOfInfandCode/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TheoryOfInfandCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[,] Baudot = new string[26, 2];
+
+        string[,] ASCII = new string[2048, 2];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Baudot[0, 0] = "A";
+            Baudot[0, 1] = "00011";
+            Baudot[1, 0] = "B";
+            Baudot[1, 1] = "11001";
+            Baudot[2, 0] = "C";
+            Baudot[2, 1] = "01110";
+            Baudot[3, 0] = "D";
+            Baudot[3, 1] = "01001";
+            Baudot[4, 0] = "E";
+            Baudot[4, 1] = "00001";
+            Baudot[5, 0] = "F";
+            Baudot[5, 1] = "01101";
+            Baudot[6, 0] = "G";
+            Baudot[6, 1] = "11010";
+            Baudot[7, 0] = "H";
+            Baudot[7, 1] = "10100";
+            Baudot[8, 0] = "I";
+            Baudot[8, 1] = "00110";
+            Baudot[9, 0] = "J";
+            Baudot[9, 1] = "01011";
+            Baudot[10, 0] = "K";
+            Baudot[10, 1] = "01111";
+            Baudot[11, 0] = "L";
+            Baudot[11, 1] = "10010";
+            Baudot[12, 0] = "M";
+            Baudot[12, 1] = "11100";
+            Baudot[13, 0] = "N";
+            Baudot[13, 1] = "01100";
+            Baudot[14, 0] = "O";
+            Baudot[14, 1] = "11000";
+            Baudot[15, 0] = "P";
+            Baudot[15, 1] = "10110";
+            Baudot[16, 0] = "Q";
+            Baudot[16, 1] = "10111";
+            Baudot[17, 0] = "R";
+            Baudot[17, 1] = "01010";
+            Baudot[18, 0] = "S";
+            Baudot[18, 1] = "00101";
+            Baudot[19, 0] = "T";
+            Baudot[19, 1] = "10000";
+            Baudot[20, 0] = "U";
+            Baudot[20, 1] = "00111";
+            Baudot[21, 0] = "V";
+            Baudot[21, 1] = "11110";
+            Baudot[22, 0] = "W";
+            Baudot[22, 1] = "10011";
+            Baudot[23, 0] = "X";
+            Baudot[23, 1] = "11101";
+            Baudot[24, 0] = "Y";
+            Baudot[24, 1] = "10101";
+            Baudot[25, 0] = "Z";
+            Baudot[25, 1] = "10001";
+            Baudot[25, 0] = " ";
+            Baudot[25, 1] = "00100";
+            string temp;
+            for (int i = 0; i < 2048; i++)
+            {
+                temp = new string((char)i, 1);
+                ASCII[i, 0] = temp;
+                ASCII[i, 1] = ToBin(i);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double AvLenCodeComb = 0;
+            double allinf = 0;
+            double enthropy = 0;
+            double fr;
+            string s = "";
+            string[,] frequency;
+
+            s = textBox3.Text;
+            Sentence sen = new Sentence(s);
+            frequency = sen.frequency;
+
+            for (int i = 0; i < sen.powerOfAlphabet; i++)
+            {
+                fr = Convert.ToDouble(frequency[i, 1]) / sen.numSymbols;
+                allinf += Convert.ToDouble(frequency[i, 1]) * -1 * fr * Math.Log(fr, 2);
+                enthropy += -1 * fr * Math.Log(fr, 2);
+                AvLenCodeComb += 11 * fr;
+
+                dataGridView1.Rows.Add(frequency[i, 0], frequency[i, 1], Math.Round(-1 * fr * Math.Log(fr, 2), 2));
+            }
+            label2.Text = Math.Round(enthropy, 2).ToString();
+            label3.Text = Math.Round(allinf, 2).ToString();
+            label5.Text = Math.Round(AvWordLen(sen), 2).ToString();
+            label7.Text = ((double)Math.Log(sen.powerOfAlphabet, 2) / enthropy).ToString();
+            label9.Text = (enthropy / AvLenCodeComb).ToString();  // энтропия / средняя длина кодовой комбинации
+            textBox1.Text = CodeASCII(s);
+            textBox2.Text = DecodeASCII(textBox1.Text);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            string s = "";
+            string[,] frequency;
+
+
+            s = textBox3.Text;
+            Sentence sen = new Sentence(s);
+            frequency = sen.frequency;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double AvLenCodeComb = 0;
+            double allinf = 0;
+            double enthropy = 0;
+            double fr;
+            string s = "";
+            string[,] frequency;
+
+            s = textBox3.Text;
+            Sentence sen = new Sentence(s);
+            frequency = sen.frequency;
+
+            for (int i = 0; i < sen.powerOfAlphabet; i++)
+            {
+                fr = Convert.ToDouble(frequency[i, 1]) / sen.numSymbols;
+                allinf += Convert.ToDouble(frequency[i, 1]) * -1 * fr * Math.Log(fr, 2);
+                enthropy += -1 * fr * Math.Log(fr, 2);
+                AvLenCodeComb += 5 * fr;
+
+                dataGridView1.Rows.Add(frequency[i, 0], frequency[i, 1], Math.Round(-1 * fr * Math.Log(fr, 2), 2));
+            }
+            label2.Text = Math.Round(enthropy, 2).ToString();
+            label3.Text = Math.Round(allinf, 2).ToString();
+            label5.Text = Math.Round(AvWordLen(sen), 2).ToString();
+            label7.Text = ((double)Math.Log(sen.powerOfAlphabet, 2) / enthropy).ToString();
+            label9.Text = (enthropy / AvLenCodeComb).ToString();  // энтропия / средняя длина кодовой комбинации
+            textBox1.Text = CodeBaudot(s);
+            textBox2.Text = DecodeBaudot(textBox1.Text);
+        }
+        string CodeASCII(string s)
+        {
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < 2048; j++)
+                {
+                    if (new string(s[i], 1) == ASCII[j, 0])
+                    {
+                        result += ASCII[j, 1];
+                    }
+                }
+            }
+            return result;
+        }
+        string DecodeASCII(string s)
+        {
+            string result = "";
+            string temp = s;
+            char[] bin1 = new char[11];
+            string bin2;
+            for (int i = 0; ; i++)
+            {
+                temp.CopyTo(0, bin1, 0, 11);
+                bin2 = new string(bin1);
+                for (int j = 0; j < 2048; j++)
+                {
+                    if (bin2 == ASCII[j, 1])
+                    {
+                        result += ASCII[j, 0];
+                    }
+                }
+                temp = temp.Remove(0, 11);
+                if (temp == "")
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+        string CodeBaudot(string s)
+        {
+
+            string result = "";
+            try
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    for (int j = 0; j < 26; j++)
+                    {
+
+                        if (new string(s[i], 1) == Baudot[j, 0])
+                        {
+                            result += Baudot[j, 1];
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некоторых введенных символов нет в алфавите Baudot");
+            }
+
+            return result;
+        }
+        string DecodeBaudot(string s)
+        {
+            string result = "";
+            string temp = s;
+            char[] bin1 = new char[5];
+            string bin2;
+            for (int i = 0; ; i++)
+            {
+                temp.CopyTo(0, bin1, 0, 5);
+                bin2 = new string(bin1);
+                for (int j = 0; j < 26; j++)
+                {
+                    if (bin2 == Baudot[j, 1])
+                    {
+                        result += Baudot[j, 0];
+                    }
+                }
+                temp = temp.Remove(0, 5);
+                if (temp == "")
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+        string ToBin(int t)
+        {
+            string temp = "";
+            for (int i = 0; i < 11; i++)
+            {
+                temp += t % 2;
+                t /= 2;
+            }
+            return Reverse(temp);
+        }
+        static string Reverse(string str)
+        {
+            char[] dop;
+            char simv;
+            int i;
+            dop = str.ToCharArray();
+            for (i = 0; i < (dop.Length / 2); i++)
+            {
+                simv = dop[i];
+                dop[i] = dop[dop.Length - i - 1];
+                dop[dop.Length - i - 1] = simv;
+
+            }
+            str = new string(dop);
+            return str;
+        }
+        static double AvWordLen(Sentence s)
+        {
+            var mas = s.messege.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            double length = 0;
+            double count = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                length += mas[i].Length;
+                count++;
+            }
+            return length / count;
+        }
+        public class Sentence
+        {
+
+            public string messege { get; set; }
+            public List<char> alphabet
+            {
+                get
+                {
+                    List<char> mes = new List<char> { };
+
+                    for (int i = 0; i < messege.Length; i++)
+                    {
+                        mes.Add(messege[i]);
+                    }
+                    IEnumerable<char> alfhabet1 = mes.Distinct();
+                    List<char> alphabet = alfhabet1.ToList();
+                    return alphabet;
+                }
+            }
+            public int numSymbols => messege.Length;
+            public int powerOfAlphabet => alphabet.Count;
+            public Sentence(string mes)
+            {
+                messege = mes;
+            }
+
+            public int CountWords()
+            {
+                int count = 0;
+                for (int i = 0; i < messege.Length && i != messege.Length - 1; i++)
+                {
+                    if (messege[i] == ' ')
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+            public string[,] frequency
+            {
+                get
+                {
+                    double countSymb = 0;
+                    string[,] fr = new string[alphabet.Count, 2];
+                    for (int i = 0; i < alphabet.Count; i++)
+                    {
+                        for (int j = 0; j < messege.Length; j++)
+                        {
+                            if (alphabet[i] == messege[j])
+                                countSymb++;
+                        }
+                        fr[i, 0] = alphabet[i].ToString();
+                        fr[i, 1] = (countSymb).ToString();
+                        countSymb = 0;
+                    }
+                    return fr;
+                }
+            }
+        }
+
+    }
+}
